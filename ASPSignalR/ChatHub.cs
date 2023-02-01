e418103242a6107876e2357595c400f95758b0ad
riazonbin <riazonbin@mail.ru>
2023-02-01 07:57:16 +0300

    changed project structure

---
+++ ASPSignalR/ChatHub.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace SignalRTest
+{
+    public class ChatHub : Hub
+    {
+        public async Task Send(string message, string username, string email)
+        {
+            await this.Clients.All.SendAsync("Receive", message, username, email);
+        }
+    }
+}
